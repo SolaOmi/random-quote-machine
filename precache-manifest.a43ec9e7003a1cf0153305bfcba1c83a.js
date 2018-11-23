ada7259f88c0f43548350fab0b02679a2dc1e5ea
bbbruno <solaocodes@gmail.com>
2018-11-23 15:28:06 -0500

    Updates

---
+++ precache-manifest.a43ec9e7003a1cf0153305bfcba1c83a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "67cb221aa913c836db13",
+    "url": "/random-quote-machine/static/js/main.67cb221a.chunk.js"
+  },
+  {
+    "revision": "09689009d67fed834754",
+    "url": "/random-quote-machine/static/js/1.09689009.chunk.js"
+  },
+  {
+    "revision": "67cb221aa913c836db13",
+    "url": "/random-quote-machine/static/css/main.468e4690.chunk.css"
+  },
+  {
+    "revision": "a2038f9b95b334daf691eedd87f0c450",
+    "url": "/random-quote-machine/index.html"
+  }
+];
