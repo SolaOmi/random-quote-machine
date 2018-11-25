56c9981be7166e23fee2f35b0b9215d58f78bfee
bbbruno <solaocodes@gmail.com>
2018-11-24 22:20:31 -0500

    Maintain necessary data in QuoteBox and pass data as needed to child components.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,29 @@ import './index.css';
 function Quote(props) {
   return (
       <div>
-        <p>
-          Two roads diverged in a wood, and I--I took the one less traveled
-          by, And that has made all the difference.
-        </p>
-        <p>-Robert Frost</p>
+        <p>{props.text}</p>
+        <p>-{props.author}</p>
       </div>
   );
 }
 
 class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: `Two roads diverged in a wood, and I--I took the one less traveled
+      by, And that has made all the difference.`,
+      author: "Robert Frost"
+    };
+  }
+
   render() {
     return (
       <div id="quote-box" className="white-background box rounded">
-        <Quote />
+        <Quote
+          author={this.state.author}
+          text={this.state.text}
+        />
       </div>
     );
   }
