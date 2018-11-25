9f39350f96e4e05451486bc2c0dcbc6e6cbae745
bbbruno <solaocodes@gmail.com>
2018-11-24 22:10:47 -0500

    Create quote function component and add it to the Quotebox component.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Quote(props) {
+  return (
+      <div>
+        <p>
+          Two roads diverged in a wood, and I--I took the one less traveled
+          by, And that has made all the difference.
+        </p>
+        <p>-Robert Frost</p>
+      </div>
+  );
+}
+
 class QuoteBox extends React.Component {
   render() {
-    return <div id="quote-box" className="white-background box rounded"></div>;
+    return (
+      <div id="quote-box" className="white-background box rounded">
+        <Quote />
+      </div>
+    );
   }
 }
 
